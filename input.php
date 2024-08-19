9f9f9c109562fd3f943ee139646e7668b8867cb3
lisna <lisnawatispentira@gmail.com>
2024-08-19 14:48:32 +0700

    frist commit

---
+++ input.php
@@ -0,0 +1,52 @@
+<?php
+
+$usia = 10;
+
+if ($usia >= 0 && $usia <= 12) {
+    echo " usia: Anak-anak";
+} elseif ($usia >= 13 && $usia <= 17) {
+    echo " usia: Remaja";
+} elseif ($usia >= 18 && $usia <= 59) {
+    echo "usia: Dewasa";
+} else {
+    echo "usia: Lansia";
+}
+
+echo "<br>";
+$usia = 16;
+
+if ($usia >= 0 && $usia <= 12) {
+    echo " usia: Anak-anak";
+} elseif ($usia >= 13 && $usia <= 17) {
+    echo " usia: Remaja";
+} elseif ($usia >= 18 && $usia <= 59) {
+    echo "usia: Dewasa";
+} else {
+    echo "usia: Lansia";
+}
+
+echo "<br>";
+$usia = 20;
+
+if ($usia >= 0 && $usia <= 12) {
+    echo " usia: Anak-anak";
+} elseif ($usia >= 13 && $usia <= 17) {
+    echo " usia: Remaja";
+} elseif ($usia >= 18 && $usia <= 59) {
+    echo "usia: Dewasa";
+} else {
+    echo "usia: Lansia";
+}
+
+echo "<br>";
+$usia = 100;
+
+if ($usia >= 0 && $usia <= 12) {
+    echo " usia: Anak-anak";
+} elseif ($usia >= 13 && $usia <= 17) {
+    echo " usia: Remaja";
+} elseif ($usia >= 18 && $usia <= 59) {
+    echo "usia: Dewasa";
+} else {
+    echo "usia: Lansia";
+}
